c693af5ccd61f7ba74b50f37f824c6cf57e65655
Stanislav Ch. Nikolov <stanislav@cukii.tk>
2016-08-18 19:52:17 +0300

    Add fruit salad

--- display.py
+++ display.py
@@ -20,16 +20,35 @@ def getTermSize():
 
     return int(line)
 
+def setColor(char):
+    if char == 'r':
+        print('[31m', end='')
+    elif char == 'g':
+        print('[32m', end='')
+    else:
+        print('[39m', end='')
+
+
 cols = getTermSize()
+
 for post in thread:
     #date = datetime.date.fromtimestamp(post['time']).strftime('%F %T')
     date = post["now"]
 
+    setColor('d')
     print('╭─', end='')
-    print('[31m', date, '[39m', end='')
-    print('─', end='')
-    print('[31m', post['no'], '[39m', end='')
-    print('─' * (cols - len(str(post['no'])) - len(date) - 8 ), end='')
+
+    setColor('d')
+    print(' ' + date + ' ', end='')
+
+    setColor('d')
+    print('──', end='')
+
+    setColor('r')
+    print(' ' + str(post['no']) + ' ', end='')
+
+    setColor('d')
+    print('─' * (cols - len(str(post['no'])) - len(date) - 9 ), end='')
     print('╮')
 
     if 'com' not in post:
@@ -41,13 +60,28 @@ for post in thread:
     for line in fixCom(post['com']).split('\n'):
         curr = 0
         end = len(line)
+
+        color = 'd'
+        if end > 1 and line[0] == '>' and line[1] == '>':
+            color = 'r'
+        elif end > 1 and line[0] == '>' and line[1] != '>':
+            color = 'g'
+
         while curr < end:
+            setColor('d')
             print('│', end='')
+
+            setColor(color)
             print(line[curr:curr+cols-2], end='')
+
             if curr + cols >= end:
                 print(' ' * (cols - end + curr - 2), end='')
+
+            setColor('d')
             print('│')
+
             curr = curr + cols - 2
 
+    setColor('d')
     print('╰' + '─' * (cols - 2) + '╯')
     print()
